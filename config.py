17d1494f7c145aebdb30422137e66345dcce0fe7
Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>
2024-08-13 20:53:46 +0530

    Update config.py
    Signed-off-by: Rajputshivsingh65 <166673972+Rajputshivsingh65@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/DNS_NETWORK",
+    "https://github.com/Rajputshivsingh65/MISHXV2",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
