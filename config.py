84534778227799a1519658be4426cd99449b0796
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-08-16 15:38:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/KiritoxAsuna02/URUxMUSICS",
+    "https://github.com/",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
