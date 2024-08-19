d51bdae258b3cce21fb43fbff16e75aedffedfb0
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-08-19 16:46:14 +0530

    Update auth.py

--- AnonXMusic/plugins/admins/auth.py
+++ AnonXMusic/plugins/admins/auth.py
@@ -1,16 +1,16 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShikhaxMusic import app
-from ShikhaxMusic.utils import extract_user, int_to_alpha
-from ShikhaxMusic.utils.database import (
+from AnonXMusic import app
+from AnonXMusic.utils import extract_user, int_to_alpha
+from AnonXMusic.utils.database import (
     delete_authuser,
     get_authuser,
     get_authuser_names,
     save_authuser,
 )
-from ShikhaxMusic.utils.decorators import AdminActual, language
-from ShikhaxMusic.utils.inline import close_markup
+from AnonXMusic.utils.decorators import AdminActual, language
+from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS, adminlist
 
 
