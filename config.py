126d1ada87c1af98e34571fc5a06b0e7ec8ce022
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-09-25 16:14:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/qW.jpg"
+    "START_IMG_URL", "https://envs.sh/q6W.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/q6W.jpg"
