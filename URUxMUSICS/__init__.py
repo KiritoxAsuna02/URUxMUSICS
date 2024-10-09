8899fd97eac227b376b31b8725d59dda76a008f0
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-10-09 11:10:49 +0530

    Update __init__.py

--- URUxMUSICS/__init__.py
+++ URUxMUSICS/__init__.py
@@ -1,8 +1,8 @@
-from AnonXMusic.core.bot import Anony
-from AnonXMusic.core.dir import dirr
-from AnonXMusic.core.git import git
-from AnonXMusic.core.userbot import Userbot
-from AnonXMusic.misc import dbb, heroku
+from URUxMUSICS.core.bot import Anony
+from URUxMUSICS.core.dir import dirr
+from URUxMUSICS.core.git import git
+from URUxMUSICS.core.userbot import Userbot
+from URUxMUSICS.misc import dbb, heroku
 
 from .logging import LOGGER
 
