241af9d9dff1a0d775f38d0e4b88307470628bdb
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2024-10-23 11:28:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,7 +78,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://envs.sh/q6W.jpg"
+    "START_IMG_URL", "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/q6W.jpg"
