cd5279f83910994f700891090a9ba3333f2d4c6f
KiritoxAsuna02 <133078081+KiritoxAsuna02@users.noreply.github.com>
2025-01-23 15:54:58 +0530

    Add files via upload

---
+++ AnonXMusic/utils/mongo.py
@@ -0,0 +1,13 @@
+from typing import Dict, Union
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+from config import MONGO_DB_URI
+
+mongo = MongoCli(MONGO_DB_URI)
+db = mongo.AnonXMusic
+
+coupledb = db.couple
+
+
+afkdb = db.afk
